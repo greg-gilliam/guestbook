73668e7a35de58e7b5376ce4d8c0d89d62a34c53
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-12-14 15:04:36 -0800

    adding context files

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = ({children}) => {
+    const [user, setUser] = useState('')
+    return <UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>
+}
+
+const useUser = () => {
+    const context = useContext(UserContext)
+
+    if (context === undefined) {
+        throw new Error('useUser hook must be called within a UserContext Provider');
+    }
+    return context;
+}
+
+export {UserProvider, useUser};
