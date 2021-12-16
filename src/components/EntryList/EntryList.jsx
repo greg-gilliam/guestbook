b423c6d6f78025ec220c35881c679882f70df9bb
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-12-15 16:54:16 -0800

    bug smashing

--- src/components/EntryList/EntryList.jsx
+++ src/components/EntryList/EntryList.jsx
@@ -1,20 +1,20 @@
-import Entry from '../Entry/Entry'
-import {useEntries} from '../../context/EntryContext'
+import Entry from '../Entry/Entry';
+import { useEntries } from '../../context/EntryContext';
 
 export default function EntryList() {
-    const {entries} = useEntries();
+  const { entries } = useEntries();
 
-    rturn (
-        <div>
-            <ul>
-                {entries.map((entry) => {
-                    return (
-                        <li key={`${entry.name}-${entry.message}`}>
-                            <Entry entry={entry} />
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <ul>
+        {entries.map((entry) => {
+          return (
+            <li key={`${entry.name}-${entry.message}`}>
+              <Entry entry={entry} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
