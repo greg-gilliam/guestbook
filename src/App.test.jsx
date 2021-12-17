45e873e91f41934d4a4e79c09cac364a9091c292
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-12-17 11:15:35 -0800

    removed App test

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
-
-xtest('App renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Hello World');
-  expect(linkElement).toBeInTheDocument();
-});
